eb851b9d972b2a215a46147dab138f9b111d5029
moocads <info@moocads.com>
2026-07-14 10:26:04 -0400

    update

--- data/athena-products.ts
+++ data/athena-products.ts
@@ -41,4 +41,18 @@ export const featuredProducts: Product[] = [
     reviewCount: 0,
     tags: ["30 G PROTEIN", "140 CALORIES", "3 G FIBRE", "ZERO ADDED SUGAR"],
   },
+  {
+    id: "iced-cappuccino-whey-protein",
+    name: "Iced Cappuccino Flavoured Whey Protein",
+    slug: "/products/iced-cappuccino-whey-protein",
+    description:
+      "Smooth iced cappuccino flavoured whey protein for everyday fitness and recovery.",
+    size: "900 g",
+    price: "$67.99",
+    image: "/images/athena/products/iced-cap-product.jpg",
+    hoverImage: "/images/athena/hero.jpg",
+    rating: 5,
+    reviewCount: 0,
+    tags: ["30 G PROTEIN", "ICED CAPPUCCINO", "ZERO ADDED SUGAR", "GLUTEN FREE"],
+  },
 ];
